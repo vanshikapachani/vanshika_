4d71f01fd6e3991a8b3351cd978020053ed9477c
vanshika <dviyapachanigmail.com>
2023-07-04 16:54:08 +0530

    first master

---
+++ 12345.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int i,j;
+clrscr();
+//outer loop
+for(i=1;i<=5;i++){
+//inner loop
+  for(j=1;j<=5;j++){
+  printf("%d",j);
+  }
+  printf("\n");
+  }
+getch();
+}
---
+++ ABCDE.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int i,j;
+clrscr();
+//outer loop
+for(i='a';i<='e';i++){
+//inner loop
+ for (j='a';j<='e';j++){
+ printf("%c",i);
+ }
+ printf("\n");
+ }
+getch();
+}
